ceb54489d20f27096e958e181037e15279863a44
ipipp <ipipp@gmx.at>
2015-10-05 08:44:58 +0200

    fixed Imports

--- java/StarTrekMod/Gui/GuiHandler.java
+++ java/StarTrekMod/Gui/GuiHandler.java
@@ -1,13 +1,8 @@
 package StarTrekMod.Gui;
 
-import net.minecraft.client.gui.Gui;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
-import StarTrekMod.Debug.ContainerDebugWarp;
-import StarTrekMod.Debug.DebugInit;
-import StarTrekMod.Debug.GuiDebugWarp;
-import StarTrekMod.Debug.TileEntityDebugWarp;
 import cpw.mods.fml.common.network.IGuiHandler;
 
 public class GuiHandler implements IGuiHandler{
